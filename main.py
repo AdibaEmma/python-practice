cd7e457cfbad7a86ccb6866799f5b7683f9ce56f
Emmanuel Adiba <eabaagah@gmail.com>
2022-03-27 21:00:32 +0000

    bill calculator

--- main.py
+++ main.py
@@ -20,13 +20,25 @@
 # bmi = int(weight) / float(height) ** 2
 # print(bmi)
 
-# Age in weeks
-age = input("What is your current age?")
-age_int = int(age)
-expected_age = 90 - age_int
-age_in_days = expected_age * 365
-age_in_weeks = expected_age * 52
-age_in_months = expected_age * 12
+# # Age in weeks
+# age = input("What is your current age?")
+# age_int = int(age)
+# years_left = 90 - age_int
+# age_in_days = years_left * 365
+# age_in_weeks = years_left * 52
+# age_in_months = years_left * 12
+#
+# print(f"You have {age_in_days} days, {age_in_weeks} weeks and {age_in_months} months left")
 
-print(f"You have {age_in_days} days, {age_in_weeks} weeks and {age_in_months} months left")
-
+# Tip calculator
+print("Welcome to the tip calculator")
+total_bill = input("What was the total bill? ")
+total_bill_as_float = float(total_bill)
+tip = input("What percentage tip would you like to give? 10, 12 or 15? ")
+tip_as_float = int(tip) * 0.01
+total_amount_payable = total_bill_as_float * (1 + tip_as_float)
+total_people = input("How many people to split the bill? ")
+total_people_as_int = int(total_people)
+split_bill = total_amount_payable / total_people_as_int
+rounded_split_bill = round(split_bill, 2)
+print(f"Each person should pay: ${rounded_split_bill}")
