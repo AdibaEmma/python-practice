83939379c123785c2031c5f5af7101a5f07df310
Emmanuel Adiba <eabaagah@gmail.com>
2022-03-27 12:14:21 +0000

    calculate bmi

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-print("Welcome to the Band Name Generator")
+# print("Welcome to the Band Name Generator")
+#
+# city = input("which city did you grow up in?\n")
+# pet = input("What's the name of your pet?\n")
+#
+#
+# print("Your band name could be " + city + " " + pet)
 
-city = input("which city did you grow up in?\n")
-pet = input("What's the name of your pet?\n")
+# Day -2
+# two_digit_number = input("Type a two digit number: ")
+# first_number = two_digit_number[0]
+# second_number = two_digit_number[1]
+#
+# result = int(first_number) + int(second_number)
+# print(result)
 
 
-print("Your band name could be " + city + " " + pet)
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+bmi = int(weight) / float(height) ** 2
+print(bmi)
