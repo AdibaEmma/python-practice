cc526183a2bca338f3646d0f5255c2e1a3bfd28d
Emmanuel Adiba <eabaagah@gmail.com>
2022-03-27 20:36:58 +0000

    age in weeks

--- main.py
+++ main.py
@@ -15,7 +15,18 @@
 # print(result)
 
 
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
-bmi = int(weight) / float(height) ** 2
-print(bmi)
+# height = input("enter your height in m: ")
+# weight = input("enter your weight in kg: ")
+# bmi = int(weight) / float(height) ** 2
+# print(bmi)
+
+# Age in weeks
+age = input("What is your current age?")
+age_int = int(age)
+expected_age = 90 - age_int
+age_in_days = expected_age * 365
+age_in_weeks = expected_age * 52
+age_in_months = expected_age * 12
+
+print(f"You have {age_in_days} days, {age_in_weeks} weeks and {age_in_months} months left")
+
