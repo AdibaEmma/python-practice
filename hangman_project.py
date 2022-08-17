a34faa67a2347456864689340c334cc4e7168557
Emmanuel Adiba <eabaagah@gmail.com>
2022-08-17 17:23:03 +0000

    update

--- hangman_project.py
+++ hangman_project.py
@@ -1,11 +1,76 @@
 import random
 
-word_list = ["aardvark", "baboon", "camel"]
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+word_list = ["aardvark", "baboon", "badger", "bat", "bear", "beaver", "camel", "cat",
+             "clam", "cobra", "cougar", "coyote", "crow", "deer", "dog", "donkey", "duck",
+             "eagle", "ferret", "fox", "frog", "goat", "goose", "hawk", "lion", "lizard",
+             "llama", "mole", "monkey", "moose", "mouse", "mule", "newt", "otter", "owl",
+             "panda", "parrot", "pigeon", "python", "rabbit", "ram", "rat", "raven", "rhino",
+             "salmon", "seal", "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork",
+             "swan", "tiger", "toad", "trout", "turkey", "turtle", "weasel", "whale", "wolf",
+             "wombat", "zebra"]
 
 chosen_word = random.choice(word_list)
 
 # Testing code
-# print(f'Pssst, the solution is {chosen_word}')
+print(f'Pssst, the solution is {chosen_word}')
 display = []
 for _ in chosen_word:
     display.append('_')
@@ -13,6 +78,7 @@ for _ in chosen_word:
 print(display)
 
 end_of_game = False
+lives = 6
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
@@ -20,9 +86,17 @@ while not end_of_game:
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
-    print(display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    print(f"{' '.join(display)}")
     if '_' not in display:
         end_of_game = True
         print("You won!")
+    print(stages[lives])
 
 
