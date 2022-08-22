ff05ae06406d5340d4513793286bcad7a44457f4
Emmanuel Adiba <eabaagah@gmail.com>
2022-08-22 11:51:53 +0000

    calculate scores

--- blackjack_capstone_prpject/main.py
+++ blackjack_capstone_prpject/main.py
@@ -1,4 +1,6 @@
 import random
+from typing import List
+
 
 def deal_card():
     """Returns a random card from the deck"""
@@ -6,12 +8,35 @@ def deal_card():
     return random.choice(cards)
 
 
+def calculate_score(cards: List[int]):
+    """Takes a list of cards and returns the score calculated from the cards"""
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
+
 user_cards: [int] = []
 computer_cards: [int] = []
+is_game_over = False
 
 for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
-print(user_cards)
-print(computer_cards)
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+print(f" Your cards: {user_cards}, current score: {user_score}")
+print(f" Computer's first card: {computer_cards[0]}")
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
+else:
+    user_should_deal = input("Type 'y' to get another card, type 'n' to pass")
+    if user_should_deal == "y":
+        user_cards.append(deal_card())
+    else:
+        is_game_over = True
