a0dbd34e5021edc4a076f3df20d9ef227bb2df8a
Emmanuel Adiba <eabaagah@gmail.com>
2022-08-30 08:38:25 +0000

    refactor

--- higher_lower_game/main.py
+++ higher_lower_game/main.py
@@ -1,32 +1,54 @@
+import random
+import os
+
 from art import logo, vs
 from game_data import data
+
+
+def format_data(account):
+    """Takes the account data and returns the printable format."""
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_descr}, from {account_country}"
+
+
+def check_answer(guess, a_followers, b_followers):
+    """Take the user guess and the follower counts and returns if they got it right."""
+    if a_followers > b_followers:
+        return guess == "A"
+    else:
+        return guess == "B"
 
 
 print(logo)
 guessed_right = True
 score = 0
-i = 0
+account_b = random.choice(data)
+
 
 while guessed_right:
-    optionA = data[i]
-    print(f"Compare A: {optionA['name']}, {optionA['description']}")
+    account_a = account_b
+    account_b = random.choice(data)
+
+    while account_a == account_b:
+        account_b = random.choice(data)
+
+    print(f"Compare A: {format_data(account_a)}")
     print(vs)
-    optionB = data[i + 1]
-    print(f"Against B: {optionB['name']}, {optionB['description']}")
+    print(f"Against B: {format_data(account_b)}")
 
-    chosen_option = input("Who has more followers? Type 'A' or 'B': ")
+    guess = input("Who has more followers? Type 'A' or 'B': ")
 
-    if chosen_option == "A":
-        if optionA["follower_count"] > optionB["follower_count"]:
-            score += 1
-            i += 1
-        else:
-            guessed_right = False
-            print(f"Sorry, that's wrong. Final score: {score}")
-    elif chosen_option == "B":
-        if optionB["follower_count"] > optionA["follower_count"]:
-            score += 1
-            i += 1
-        else:
-            guessed_right = False
-            print(f"Sorry, that's wrong. Final score: {score}")
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+
+    is_correct = check_answer(guess, a_followers=a_follower_count, b_followers=b_follower_count)
+    
+    os.system("cls")
+    if is_correct:
+        score += 1
+        print(f"You are right! Current score: {score}")
+    else:
+        print(f"Sorry, that's wrong. Final score: {score}")
+        guessed_right = False
