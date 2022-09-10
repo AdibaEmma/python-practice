1cd092ab7799166215d9b904f5058a35c33ad3a4
Emmanuel Adiba <eabaagah@gmail.com>
2022-09-10 09:16:09 +0000

    add spirograph

--- oop/main.py
+++ oop/main.py
@@ -31,6 +31,7 @@
 
 from turtle import Turtle, Screen
 from random import randint
+from typing import Tuple
 
 timmy = Turtle()
 
@@ -45,19 +46,36 @@ timmy = Turtle()
 #     timmy.forward(10)
 #     timmy.pendown()
 
-def draw_shape(num_sides):
-    for i in range(num_sides):
-        angle = 360 / num_sides
-        timmy.forward(100)
-        timmy.right(angle)
+# def draw_shape(num_sides):
+#     for _ in range(num_sides):
+#         angle = 360 / num_sides
+#         timmy.forward(100)
+#         timmy.right(angle)
 
 
-for i in range(3, 11):
+def random_color() -> Tuple[int, int, int]:
     r = randint(0, 1)
     g = randint(0, 1)
     b = randint(0, 1)
-    timmy.pencolor(r, g, b)
-    draw_shape(i)
+    color = (r, g, b)
+    return color
 
+
+# for i in range(3, 11):
+#     random_color = random_color()
+#     timmy.color(random_color)
+#     draw_shape(i)
+
+timmy.speed("fastest")
+
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        timmy.color(random_color())
+        timmy.circle(100)
+        timmy.setheading(timmy.heading() + size_of_gap)
+
+
+draw_spirograph(1)
 my_screen = Screen()
 my_screen.exitonclick()
