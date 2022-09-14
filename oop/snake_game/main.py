ce5f12e33433f22a28fb15272df297250e52acc4
Emmanuel Adiba <eabaagah@gmail.com>
2022-09-14 08:14:11 +0000

    add snake body

--- oop/snake_game/main.py
+++ oop/snake_game/main.py
@@ -5,7 +5,12 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
-
+position_x = 0
+for turtle_body in range(0, 3):
+    new_turtle = Turtle(shape="square")
+    new_turtle.color("white")
+    new_turtle.goto(x=position_x, y=0)
+    position_x -= 20
 
 
 
