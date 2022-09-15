8ec391396bb5365a82a5c41b4e9fc34f9ed2dbda
Emmanuel Adiba <eabaagah@gmail.com>
2022-09-15 20:52:05 +0000

    create food object

--- oop/snake_game/main.py
+++ oop/snake_game/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,6 +10,8 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -20,7 +23,6 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
 
