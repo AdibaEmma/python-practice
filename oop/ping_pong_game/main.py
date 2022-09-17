cc12ed2242daf23d26b960ad0a7f178b79b45cc4
Emmanuel Adiba <eabaagah@gmail.com>
2022-09-17 11:35:48 +0000

    create two paddles

--- oop/ping_pong_game/main.py
+++ oop/ping_pong_game/main.py
@@ -5,10 +5,21 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+screen.tracer(0)
 
-paddle = Paddle()
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkey(paddle.up, "Up")
-screen.onkey(paddle.down, "Down")
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+
+screen.update()
 screen.exitonclick()
--- oop/ping_pong_game/paddle.py
+++ oop/ping_pong_game/paddle.py
@@ -1,24 +1,24 @@
 from turtle import Turtle
-
+from typing import Tuple
 
 MOVE_DISTANCE = 20
 
 
 class Paddle(Turtle):
 
-    def __init__(self):
+    def __init__(self, coordinate: Tuple[float, float]):
         super().__init__()
         self.shape("square")
         self.color("white")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
-        self.goto(x=350, y=0)
+        self.goto(coordinate)
 
     def up(self):
         new_y = self.ycor() + MOVE_DISTANCE
-        self.goto(x=350, y=new_y)
+        self.goto(self.xcor(), y=new_y)
         
     def down(self):
         new_y = self.ycor() - MOVE_DISTANCE
-        self.goto(x=350, y=new_y)
+        self.goto(self.xcor(), y=new_y)
 
