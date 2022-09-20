448801a1e9d0d78c36c372fc0a939e1d0746cc23
Emmanuel Adiba <eabaagah@gmail.com>
2022-09-20 13:27:34 +0000

    create scoreboard

--- oop/turtle-crossing/main.py
+++ oop/turtle-crossing/main.py
@@ -10,6 +10,7 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -27,5 +28,6 @@ while game_is_on:
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
+        scoreboard.increase_level()
 
 screen.exitonclick()
