9fcf9ecb06ab71640ef209cf53eb07c77bfc3b88
Emmanuel Adiba <eabaagah@gmail.com>
2022-09-22 17:40:59 +0000

    create letter for each invited names

--- file_system/main.py
+++ file_system/main.py
@@ -13,3 +13,17 @@
 # Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
 # Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
 # Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("./Input/Names/invited_names.txt", "r") as file:
+    names_to_invite = file.readlines()
+
+with open("./Input/Letters/starting_letter.txt", "r") as file:
+    letter_contents = file.readlines()
+    for name in names_to_invite:
+        formatted_name = name.strip("\n")
+        greetings = letter_contents[0].replace("[name]", f"{formatted_name}")
+        with open(f"./Output/ReadyToSend/{formatted_name.lower()}.txt", "w") as file:
+            for content in letter_contents:
+                if content == letter_contents[0]:
+                    content = greetings
+                file.write(content)
