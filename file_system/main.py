a412a7d462ef9f7a4f5c56847d25048975b0f3e2
Emmanuel Adiba <eabaagah@gmail.com>
2022-09-22 15:53:30 +0000

    mail merge starter

--- file_system/main.py
+++ file_system/main.py
@@ -2,5 +2,14 @@
 #     contents = file.read()
 #     print(contents)
 
-with open("new_file.txt", mode="w") as file: # Create the file if not exists in write mode
-    file.write("New text.")
+# with open("new_file.txt", mode="w") as file: # Create the file if not exists in write mode
+#     file.write("New text.")
+
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
