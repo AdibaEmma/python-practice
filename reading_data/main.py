6b80cd02c028cb6d046d50c5e00f550fbd3ad067
Emmanuel Adiba <eabaagah@gmail.com>
2022-09-27 04:39:47 +0000

    squirrels count by color

--- reading_data/main.py
+++ reading_data/main.py
@@ -27,10 +27,29 @@ import pandas
 # print(monday_temp_F)
 
 # Create a dataframe from scratch
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+# 
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+red_squirrels = data[data["Primary Fur Color"] == "Cinnamon"]
+red_squirrels_count = len(red_squirrels["Hectare Squirrel Number"])
+
+gray_squirrels = data[data["Primary Fur Color"] == "Gray"]
+gray_squirrels_count = len(gray_squirrels["Hectare Squirrel Number"])
+
+black_squirrels = data[data["Primary Fur Color"] == "Black"]
+black_squirrels_count = len(black_squirrels["Hectare Squirrel Number"])
+
 data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 65]
+    "Fur Color": ["grey", "red", "black"],
+    "Count": [gray_squirrels_count, red_squirrels_count, black_squirrels_count]
 }
 
-data = pandas.DataFrame(data_dict)
-data.to_csv("new_data.csv")
+squirrel_count_data = pandas.DataFrame(data_dict)
+squirrel_count_data.to_csv("squirrel_count.csv")
