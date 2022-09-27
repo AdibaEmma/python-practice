2ab60afed4129696982b5f2f5a5c7719f347bb9c
Emmanuel Adiba <eabaagah@gmail.com>
2022-09-27 04:03:04 +0000

    get data in rows

--- reading_data/main.py
+++ reading_data/main.py
@@ -1,4 +1,28 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+# print(type(data))
+# # print(data["temp"])
+# # print(type(data["temp"]))
+#
+# data_dict = data.to_dict()
+# # print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+#
+#
+# print(data["temp"].mean())
+# print(data["temp"].max())
+#
+# # Get Data in Columns
+# print(data["condition"])
+# print(data.condition)
+
+# Get Data in Row
+# print(data[data.day == "Monday"])
+
+# print(data[data.temp == data.temp.max()])
+monday = data[data.day == "Monday"]
+monday_temp_F = monday.temp * 9 / 5 + 32
+print(monday_temp_F)
+
