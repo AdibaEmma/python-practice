970b54997d3ace0df830797550df5e7cbeae22a3
Emmanuel Adiba <eabaagah@gmail.com>
2022-09-28 10:26:14 +0000

    code refactor

--- reading_data/us-states-game/main.py
+++ reading_data/us-states-game/main.py
@@ -8,16 +8,18 @@ screen.addshape(image)
 turtle.shape(image)
 
 states_data = pandas.read_csv("50_states.csv")
-
 all_states = states_data.state.to_list()
-lower_cased_states = [x.lower() for x in all_states]
 
-total_guessed_right = 0
+guessed_states = []
 total_states = len(all_states)
-while total_guessed_right < total_states:
-    answer_state = screen.textinput(title=f"{total_guessed_right}/{total_states} States Correct",
+while len(guessed_states) < total_states:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/{total_states} States Correct",
                                     prompt="What's another state's name?").title()
-    if answer_state.lower() in lower_cased_states:
+
+    if answer_state == "Exit":
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
         state = states_data[states_data.state == answer_state]
         coordinates = (int(state.x), int(state.y))
         new_turtle = turtle.Turtle()
@@ -25,7 +27,6 @@ while total_guessed_right < total_states:
         new_turtle.penup()
         new_turtle.goto(coordinates)
         new_turtle.write(answer_state.title())
-        total_guessed_right += 1
 
 
 turtle.mainloop()
