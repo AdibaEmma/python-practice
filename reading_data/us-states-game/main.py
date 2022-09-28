3b1fcab6769082e380addd8f00f2b4927a38603c
Emmanuel Adiba <eabaagah@gmail.com>
2022-09-28 09:59:06 +0000

    guess the state

---
+++ reading_data/us-states-game/main.py
@@ -0,0 +1,32 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+states_data = pandas.read_csv("50_states.csv")
+
+all_states = states_data.state.to_list()
+lower_cased_states = [x.lower() for x in all_states]
+
+total_guessed_right = 0
+total_states = len(all_states)
+while total_guessed_right < total_states:
+    answer_state = screen.textinput(title=f"Total guessed {total_guessed_right}/{total_states}",
+                                    prompt="What's another state's name?")
+    if answer_state.lower() in lower_cased_states:
+        state = states_data[states_data.state == answer_state.title()]
+        coordinates = (int(state.x), int(state.y))
+        new_turtle = turtle.Turtle()
+        new_turtle.hideturtle()
+        new_turtle.penup()
+        new_turtle.goto(coordinates)
+        new_turtle.write(answer_state.title())
+        total_guessed_right += 1
+
+
+turtle.mainloop()
+
