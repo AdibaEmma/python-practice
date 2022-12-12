65506c6226b123c7b78974c2dd8d9ebdf5a9bd5d
Emmanuel Adiba <eabaagah@gmail.com>
2022-12-12 11:01:32 +0000

    update ui

--- gui/pomodoro/main.py
+++ gui/pomodoro/main.py
@@ -19,12 +19,23 @@ LONG_BREAK_MIN = 20
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
+label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 40, "bold"), bg=YELLOW)
+label.grid(column=1, row=0)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+
+start_button = Button(text="Start")
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset")
+reset_button.grid(column=2, row=2)
+
+check_label = Label(text="✔", fg=GREEN, font=(FONT_NAME, 12, "bold"), bg=YELLOW)
+check_label.grid(column=1, row=3)
+canvas.grid(column=1, row=1)
 
 
 
