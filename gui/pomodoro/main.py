1b762b2ab1570e73f9a28e65c94b7c26e81c25c5
Emmanuel Adiba <eabaagah@gmail.com>
2022-12-13 14:47:04 +0000

    add timer func to canvas

--- gui/pomodoro/main.py
+++ gui/pomodoro/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,11 +10,17 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -26,8 +33,10 @@ title_label.grid(column=1, row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
+
+count_down(5)
 
 start_button = Button(text="Start", highlightthickness=0)
 start_button.grid(column=0, row=2)
@@ -37,10 +46,5 @@ reset_button.grid(column=2, row=2)
 
 check_label = Label(text="✔", fg=GREEN, font=(FONT_NAME, 12, "bold"), bg=YELLOW)
 check_label.grid(column=1, row=3)
-
-
-
-
-
 
 window.mainloop()
