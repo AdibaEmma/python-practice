367d2746849c2fb9c0c1935a29146851c1f4c3d4
Emmanuel Adiba <eabaagah@gmail.com>
2022-12-14 11:08:46 +0000

    update

--- gui/pomodoro/main.py
+++ gui/pomodoro/main.py
@@ -7,7 +7,7 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
+WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
@@ -18,6 +18,10 @@ timer = None
 def reset_timer():
     window.after_cancel(timer)
     canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    check_label.config(text="")
+    global reps
+    reps = 0
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
